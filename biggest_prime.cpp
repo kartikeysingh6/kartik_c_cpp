963082774f17ed325ff7c9f898424c66a3eeb67f
Kartikey Singh <59006007+kartikeysingh6@users.noreply.github.com>
2020-02-17 11:06:39 +0530

    Update and rename biggest_prime to biggest_prime.cpp

--- biggest_prime.cpp
+++ biggest_prime.cpp
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include<stdio.h>  //this program prints the prime number equal to or less than the given no.
 #include<math.h>
 int main(){ 
-int n,c,a;
+int n,factors,a;
 printf("Enter the number: ");
 scanf("%d",&n);
 for(int i=1;i<=n;+i++){
-    c=0;
+    factors=0;
     for(int j=1;j<=i;j++){
         if(i%j==0)
-        c++;}
-    if(c==2)
+        factors++;}
+    if(factors==2)
     a=i;}
 printf("%d ",a);
 return 0;}
